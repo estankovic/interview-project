2a4d48accd811dae99adae1ad29563ea5cb78807
Eduard Stankovic <eduard.stankovic.ext@uniqa.at>
2021-04-30 13:06:07 +0200

    Added target info to list

--- src/app/shared/components/activity-tile/activity-tile.component.ts
+++ src/app/shared/components/activity-tile/activity-tile.component.ts
@@ -3,7 +3,9 @@ import {Component, Input, OnInit} from '@angular/core';
 @Component({
   selector: 'app-activity-tile',
   templateUrl: './activity-tile.component.html',
+
   styleUrls: ['./activity-tile.component.scss'],
+
 })
 export class ActivityTileComponent implements OnInit {
 
@@ -13,8 +15,17 @@ export class ActivityTileComponent implements OnInit {
 
   @Input() progress: number;
 
+  @Input() target: number;
+
   constructor() { }
 
   ngOnInit() {}
 
+  calculateMeterToKMIfNecessary(value) {
+    if (value < 1000) {
+      return value.toFixed(1) + ' m';
+    } else {
+      return (value / 1000).toFixed(1) + 'km';
+    }
+  }
 }
